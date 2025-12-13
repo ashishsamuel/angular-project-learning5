659601e430d5d7045254fb2759f5704bd83ce830
ashishsamuel <ashishsamuelthomson@gmail.com>
2025-12-13 15:57:47 +0530

    template driven form component demo changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { FormsDemoComponent } from './forms-demo/forms-demo.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
-  { path: 'forms-demo', component: FormsDemoComponent}
+  { path: 'forms-demo', component: FormsDemoComponent},
+  { path: 'template-driven-form', component: TemplateDrivenFormComponent}
 ];
---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './template-driven-form.component.html',
+  styleUrl: './template-driven-form.component.scss'
+})
+export class TemplateDrivenFormComponent {
+
+  // @ViewChild('userForm') formValues: NgForm;
+  genderList = [
+    {id:"male", display: "Male", value: "Male"},
+    {id:"female", display: "Female", value: "Female"},
+    {id:"other", display: "Other", value: "Other"},
+  ];
+
+  uName: string = '';
+  place: string = '';
+  gender: string = '';
+
+  submitUserForm(form:NgForm){
+	  console.log("form values",form)
+    // accesing form field values from value property
+    console.log("form value of username",form.value.userName)
+    console.log("form value of place",form.value.place)
+    console.log("form value of gender",form.value.gender)
+
+    // accessing form field values from controls property
+    console.log("form value of username from controls property",form.controls['userName'].value);
+    console.log("form value of username from controls property",form.controls['place'].value);
+    console.log("form value of username from controls property",form.controls['gender'].value);
+    
+  }
+
+}
