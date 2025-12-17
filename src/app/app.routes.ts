36086855141a1c4d0f34c76f5d4dfca9c2bc7212
ashishsamuel <ashishsamuelthomson@gmail.com>
2025-12-17 10:07:24 +0530

    changes for reactive forms component creation and adding routing for the same

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { FormsDemoComponent } from './forms-demo/forms-demo.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormsDemoComponent } from './reactive-forms-demo/reactive-forms-demo.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
   { path: 'forms-demo', component: FormsDemoComponent},
-  { path: 'template-driven-form', component: TemplateDrivenFormComponent}
+  { path: 'template-driven-form', component: TemplateDrivenFormComponent},
+  { path: 'reactive-form', component: ReactiveFormsDemoComponent}
 ];
---
+++ src/app/reactive-forms-demo/reactive-forms-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-forms-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './reactive-forms-demo.component.html',
+  styleUrl: './reactive-forms-demo.component.scss'
+})
+export class ReactiveFormsDemoComponent {
+
+}
