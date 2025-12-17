a9a4f9f9dd2706b47bb7d5968323015024a3b0d4
ashishsamuel <ashishsamuelthomson@gmail.com>
2025-12-17 23:06:47 +0530

    reactive form initialization and error validation messages

--- src/app/reactive-forms-demo/reactive-forms-demo.component.ts
+++ src/app/reactive-forms-demo/reactive-forms-demo.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms-demo',
   standalone: true,
-  imports: [],
+  imports: [ɵInternalFormsSharedModule,FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-forms-demo.component.html',
   styleUrl: './reactive-forms-demo.component.scss'
 })
-export class ReactiveFormsDemoComponent {
+export class ReactiveFormsDemoComponent implements OnInit{
+
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+
+    // reactive form fields initialization
+    this.reactiveForm = new FormGroup({
+      username: new FormControl(null,Validators.required),
+      email: new FormControl(null,[Validators.required,Validators.email]),
+      gender: new FormControl(null),
+      place: new FormControl(null,Validators.required),
+      address: new FormGroup({
+        street: new FormControl(null,Validators.required),
+        district: new FormControl(null,Validators.required),
+        state: new FormControl(null,Validators.required)
+      })
+      // street: new FormControl(null),
+    })
+  }
+
+  submitReactiveForm() {
+    console.log("reactiveform",this.reactiveForm);    
+  }
 
 }
