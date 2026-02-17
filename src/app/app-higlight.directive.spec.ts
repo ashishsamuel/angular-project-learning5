a5c5ede4425b62446042d752a3e40090e8bf8601
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-02-17 09:30:12 +0530

    custom attribute directive

---
+++ src/app/app-higlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppHiglightDirective } from './app-higlight.directive';
+
+describe('AppHiglightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppHiglightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/app-higlight.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAppHiglight]',
+  standalone: true
+})
+export class AppHiglightDirective {
+
+  constructor() { }
+
+}
