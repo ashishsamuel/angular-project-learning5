6fcf47fdaf505177e1c48ad9fc90765540033258
ashishsamuel <ashishsamuelthomson@gmail.com>
2026-02-24 18:49:48 +0530

    subjects, behaviorSubject and replaySubject changes

--- src/app/forms-interview/forms-interview.component.ts
+++ src/app/forms-interview/forms-interview.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, ElementRef, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChildDemoInteviewComponent } from "../child-demo-inteview/child-demo-inteview.component";
 import { AppHighlightDirective } from '../directives/app-highlight.directive';
 import { InterviewPracticeService } from '../services/interview-practice.service';
-import { from, fromEvent, Observable, of } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, from, fromEvent, Observable, of, pipe, ReplaySubject, Subject, switchMap, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-forms-interview',
@@ -13,7 +14,7 @@ import { from, fromEvent, Observable, of } from 'rxjs';
   templateUrl: './forms-interview.component.html',
   styleUrl: './forms-interview.component.scss'
 })
-export class FormsInterviewComponent implements OnInit, AfterViewInit{
+export class FormsInterviewComponent implements OnInit, AfterViewInit, OnDestroy{
 
   fullName: string = '';
   allProductsList: any[] =[];
@@ -212,9 +213,217 @@ export class FormsInterviewComponent implements OnInit, AfterViewInit{
   // rxjs operators practice
   // map, filter, debouncetime, switchMap, mergeMap, concatMap, exhaustMap, take, takeUntil, forkJoin,
   // catchError, share, pipe, tap, interval, first, last, throttleTime, distinctUntilChanged
-  // retry
+  // retry, delay
 
+  first3Products:any[] = [];
+
+  fetchFirst3Products() {
+    this.service.getFirst3Products().subscribe({
+      next:(products)=>{
+        console.log("first 3 products",products);
+        this.first3Products = products;
+      },
+      error:(err:any)=>{
+        console.log("error inside api",err);
+      }
+    })
+  }
+
+  // basically takeuntil() is used when we want to unsubscribe automatically from a source observable when a notifier observable emits its value
+  // used mainly for avoiding memory leaks when components are destroyed
+  // we maintain a subject observable destroy$ which is our notifier and as this notifier emits values 
+  // the current subscription also will get cancelled like below
   
+  destroy$ = new Subject<void>();
+  takeUntillist: any[] = [];
+
+  takeUntilDemoFunction() {
+    this.service.takeUntilProductsDestroy().pipe(takeUntil(this.destroy$)).subscribe((productsList)=>
+    this.takeUntillist = productsList
+  )}
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  // another approach ----------------------
+  destroyRef = inject(DestroyRef);
+
+  // wihout implementing angular's destruction ngondestroy we can hook into the components destruction cycle
+  // takeuntildestroyed usually used with destroyref 
+
+  takeUntilDestroyedDemoFunction() {
+    this.service.takeUntilProductsDestroy().pipe(takeUntilDestroyed(this.destroyRef)).subscribe((productsList)=>
+    this.takeUntillist = productsList
+  )}  
+
+  // distinctuntilchanged() operator
+  duplicateEmissionObservableFunctionComp(){
+    this.service.duplicateEmissionObservableFunction().subscribe((res)=>{
+      console.log("response of not getting any duplicate datas",res);    
+    })
+  }
+  
+  // retry operator
+  retryOperator() {
+    this.service.getFirst3ProductsRetry().subscribe((data)=>{
+      console.log("if api fails due to server issue then retry the failed api for 2 times as per we have given");   
+    })
+  }
+
+  // debouncetime, distinctuntilchanged and switchmap
+
+  searchText$ = new Subject();
+
+  fetchSearchText(searchValue: string) {
+
+    this.searchText$.next(searchValue);
+    console.log("searchvalue",searchValue);
+    this.searchText$.pipe(debounceTime(3000),distinctUntilChanged(),
+    switchMap((searchTerm:string)=>
+      this.service.fetchSearchResults(searchTerm)
+    )).subscribe((res)=>{
+      console.log("response based on search text", res);
+    }
+    )
+    
+  }
+
+  // concatmap, exhaustmap, mergemap, catcherror
+
+  // mergeMap runs all the inner observables in parallel
+  // orders are not guaranteed for the execution
+  // it does not cancels the previous requests
+  // it will combine and give the result of all the emissions into a single result
+  // file download scenario 
+  mergeMapFunctionCall() {
+    let ids = [1,2,3,4];
+    this.service.mergeMapProductDetails(ids).subscribe((res)=>{
+      console.log("resultant data from mergemap", res);
+    })
+  }
+
+  // concatMap() 
+  // operator that is used when the inner obseravbles have to be handled sequentially 
+  // order is guranteed
+  // previous requests and emissions wont get cancelled
+  // sequential file upload scenrio and all
+  concatMapProductDetailsFunction() {
+    let ids = [5,6,7,8];
+    this.service.concatMapProductDetails(ids).subscribe((res)=>{
+      console.log("response from the concatmap operator",res);
+      
+    })
+  }
+
+
+  // exhaustmap operator in rxjs
+  // it will ignore all new emissions until the current emitting observable emits fully
+  // usually used in case of preventing multiple form submit clicks and button clicks 
+  exhaustMapFunctionCall() {
+    this.service.productTrigger$.next(10);
+    this.service.exhaustMapProductDetails().pipe(debounceTime(2000)).subscribe((res)=>{
+      console.log("exhaustmap function",res);
+    })
+
+    this.service.productTrigger$.next(11);
+
+    this.service.productTrigger$.next(12);
+
+  }
+
+  // subjects in angular
+
+  subject = new Subject();
+  passValueSubject() {
+    this.service.emitValueToSubject({username:'Ashish Samuel Thomson',userId:123});
+    // this.service.emitValueToSubject({username:'Ajmal Roshna',userId:198 });
+    // this.subject.next("hello ashish");
+    // this.subject.next("hello yashin");
+    // this.subject.next("hello saranya");
+
+    const subject = new Subject<number>();
+
+subject.next(1);
+subject.subscribe(value => console.log(value));
+
+subject.next(2);
+
+
+
+  }
+
+  // Without subscription ther will be no value to emit always subscription is needed for mission in subjects
+
+  // subject can act as data provider as well as data consumer
+
+  // Because a normal Subject does not store previously emitted values. It only delivers values to active subscribers. If we need late subscribers to receive the last emitted value, we should use BehaviorSubject or ReplaySubject.
+
+  // automatically subjects will notify the value has been changed like that
+  fetchSubjectValue() {
+    // this.service.emitValueToSubject({username:'Amal Raj',userId:145});
+    this.subject.subscribe((res)=>{
+      console.log("res",res);
+    })
+    this.subject.next(20);
+
+    let userValue = this.service.fetchValueFromSubject();
+    console.log("uservalue observable",userValue);
+    
+    userValue.subscribe((res)=>{
+         console.log("uservalue",res);
+      }) 
+  }
+
+  // behaviorSubject() and replaySubject()
+  // behaviorSubject -> we can assign an intial value of any type and if we dont know the type of 
+  // value we can give it as null
+  // it stores the last value that has been emitted by the observable
+
+  productName = new BehaviorSubject<string>("Shoe");
+  productName$ = this.productName.asObservable();
+
+  // function for getting the last emitted value and the the console inside this will be triggered whenever the value of the productname is being updated
+  getDataBehaviorSubject() {
+    this.productName$.subscribe((res)=>{
+      console.log("data from behvaiour subject",res);
+    })
+  }
+
+  // function for updating the value of the productName using next() method
+  emitDataBehaviorSubject() {
+    this.productName.next("Bag");
+  }
+
+  // replaySubject is also a type of subject that can store mutliple values that has been emitted
+  // we can specify the count of how many values that has been emitted needs to be stored and 
+  // also the time period also we can give for how long the values should be stored
+
+  // replaysubject will consider the buffer size which we have given only if subscription happens after emission and it wont conside buffer size and will emit all the datas if subscription happens before emission
+
+  // if we give buffer time then after that time all datas will get cleared which is the last 2 values in
+  // this case
+  
+  // student = new ReplaySubject<any>(2,1000);
+  student = new ReplaySubject<any>(2);
+
+
+  emitDataReplaySubject() {
+    this.student.next({studName:"ashish samuel", id:1});
+    setTimeout(() => {
+      this.student.next({studName:"aswin mohan", id:2});
+    }, 1000);
+    setTimeout(() => {
+      this.student.next({studName:"akash menon", id:3});
+    }, 2000);
+  }
+
+  getDataReplaySubject() {
+    this.student.subscribe((res)=>{
+      console.log("response from replay subject",res);
+    })
+  }
   
 
 }
--- src/app/services/interview-practice.service.ts
+++ src/app/services/interview-practice.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiServiceService } from '../api-service.service';
 import { HttpClient } from '@angular/common/http';
-import { filter, map, tap } from 'rxjs';
+import { concatMap, debounceTime, delay, distinctUntilChanged, exhaustMap, filter, map, mergeMap, retry, take, tap, toArray } from 'rxjs/operators';
+import { from, of, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,10 +25,12 @@ export class InterviewPracticeService {
       }
     });
   }
+  allProductsList:any[]=[];
 
     // fucntion for getting all products list 
     getAllProductsList () {
       return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(tap((allProductList)=>{
+        this.allProductsList = allProductList;
         // console.log("all products list inside tap operator", allProductList);
       }),
       map((productList:any[])=>
@@ -42,17 +45,115 @@ export class InterviewPracticeService {
     
     // function for filtering products that are having price > 100
     getProductsPrice100() {
-      return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(map((productList:any[])=>productList.filter((product)=>product.price>100)),
-      map(product=>product.map((prod)=>{
-        return {
+      return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(delay(3000),map((productList:any[])=>productList.filter((product)=>product.price>100)),
+      map(product=>product.map((prod)=>({
           productName:prod.title,
           productPrice:prod.price
-        }
-      }))
+      })
+    ))
     )
     }
+
+    // function for getting the first 3 products from the response
+    // take(n) operator will emit the first n number of values where n is th number of how many first values it should emit
+    // if the observable returns a single array then the whole array will be emitted its not like it will emit the first 3 elemnts of the array take() doesnt work like that
+
+    // getFirst3Products() {
+    //   return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(take(3));
+    // }
   
+    // take() mainly used when the observable emits data each at a time and the count which we mention inside the take will be the number of times datas will be getting emitted
+    
+    // getFirst3Products() {
+    //   return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(map((products:any[])=>
+    //    products.slice(0,3) 
+    //   ));
+    // }
+
+    // usage of take with mergemap and from operaotr 
+    getFirst3Products() {
+      return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(mergeMap(products=>from(products)),take(3),toArray()
+    );
+    }
+
+    // usage of takeUntil() operator 
+    takeUntilProductsDestroy() {
+      return this.http.get<any[]>('https://fakestoreapi.com/products');
+    }
       
+    // distinctuntilchanged()
+  // operator in rxjs used to prevent emitting duplicate consecutive values from an observable
+  duplicateEmissionObservable = from([12,12,14,13,16,17,13]);
+
+  // so in this observable the consecutive 12 wont emit as we are using distinctuntilcahnged operator
+  
+  duplicateEmissionObservableFunction() {
+    return this.duplicateEmissionObservable.pipe(distinctUntilChanged());
+  }
+
+  // retry operator
+  // due to some issues if an api call is failed then if we want to trigger the same api then we use the retry operator and we specify the count like how many times again it needs to be retried retry(3)
+  // retry operator automatically retries failed API calls
+// retry operator accepts either count or an object with count and deplay paramters if we give delay then after that much time again the api will recalled
+// retry({
+  // count:2,
+  // delay:1000
+// })
+
+// retry
+getFirst3ProductsRetry() {
+      return this.http.get<any[]>('https://fakestoreapi.com/products').pipe(retry(2),map((products:any[])=>
+       products.slice(0,3) 
+      ));
+    }
+
+    // switchmap() operator
+    // function for search api call
+    fetchSearchResults(searchtext:string) {
+      return of(this.allProductsList).pipe(map(productList=>productList.filter(product=>{
+        // console.log("products",product);
+        return product.title.toLowerCase().startsWith(searchtext.toLowerCase())
+      }
+    )))
+    }
+
+    // mergemap operator 
+    mergeMapProductDetails(ids:number[]){
+      return from(ids).pipe(
+        mergeMap(id=> this.http.get<any>(`https://fakestoreapi.com/products/${id}`)),
+        toArray()
+      )
+    }
+
+    // concatmap operator
+    concatMapProductDetails(ids:number[]) {
+      return from(ids).pipe(
+        concatMap(id=>this.http.get<any>(`https://fakestoreapi.com/products/${id}`)
+      ),toArray()
+      )
+    }
+
+    // exhaustmap operator
+    productTrigger$ = new Subject();
+    exhaustMapProductDetails() {
+      return this.productTrigger$.pipe(exhaustMap(id=>this.http.get<any>(`https://fakestoreapi.com/products/${id}`)))
+    }
+    
+
+    // catcherror and forkjoin
 
 
+    // subjects in angular
+
+    user$ = new Subject<any>();
+
+    emitValueToSubject(value:any){
+      // console.log("value",value);
+      
+      this.user$.next(value);
+    }
+
+    fetchValueFromSubject() {
+      return this.user$;
+    }
 }
